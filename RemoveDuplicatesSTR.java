74d7aa2e478ae78c7d8fb510f05ca7f8a2380d67
Krish Sharma <krishsharma9005@gmail.com>
2025-02-01 11:30:23 +0530

    Remove Duplicates in String

--- RemoveDuplicatesSTR.java
+++ RemoveDuplicatesSTR.java
@@ -18,7 +18,18 @@ public class RemoveDuplicatesSTR {
         }
         return str;
     }
+
+    public static String removeDup(String s) { //more useful
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i<s.length(); i++) {
+            int currChar = s.charAt(i);
+            if(str.indexOf(String.valueOf(currChar))==-1) {
+                str.append(currChar);
+            }
+        }
+        return str.toString();
+    }
     public static void main(String[] args) {
-        System.out.println(removeDuplicates("aabcddikf"));
+        System.out.println(removeDup("aAbcDdikf"));
     }
 }
